10d012347b546a8ebe741bd7470b91c6033b2411
dwjrgal <dwjrgl651@gmail.com>
2024-08-28 13:00:41 +0800

    env

---
+++ back-end/middlewares/logger.js
@@ -0,0 +1,7 @@
+export const logger = () => {
+  return (req, res, next) => {
+    console.log(`${req.method}: ${req.originalUrl}`);
+    next();
+  };
+};
+module.exports = { logger };
--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,30 @@
-const express = require(express);
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+const app = express();
+const { logger } = require("./middlewares/logger");
+
+app.use(cors());
+app.use(express.json());
+app.use(logger());
+
+app.get("/user", (req, res) => {
+  console.log("all user is read successfully");
+  res.json({ message: "Get success" });
+});
+app.post("/user", (req, res) => {
+  console.log("New user is created successfully");
+});
+app.put("/user", (req, res) => {
+  console.log("user is updated successfully");
+});
+app.delete("/user", (req, res) => {
+  console.log("user is deleted successfully");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running  at localhost ${PORT}`);
+});
