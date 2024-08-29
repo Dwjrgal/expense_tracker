8dbd8e567079fa58c9a6efb6db164c0b72c185b1
dwjrgal <dwjrgl651@gmail.com>
2024-08-29 12:56:16 +0800

    controllers

--- back-end/controllers/category-controller.js
+++ back-end/controllers/category-controller.js
@@ -1,15 +1,40 @@
 const getAllCategory = (req, res) => {
-    // postgre select
-    res.status(200).json({});
-  };
-  
-  const createCategory = () => {};
-  const updateCategory = () => {};
-  const deleteCategory = () => {};
-  
-  module.exports = {
-    getAllCategory,
-    createCategory,
-    updateCategory,
-    deleteCategory,
-  };
+  // postgre select
+  res.status(200).json({});
+};
+
+const createCategory = async (req, res) => {
+  const { name, description, category_image } = req.body;
+  const data =
+    await sql`INSERT INTO categories (name, description, category_image )
+  VALUES (
+  ${name},
+  ${description},
+  ${category_image}
+  );`;
+  console.log("DATA", data);
+  res.status(200).json({ message: "Create category success", user: data });
+};
+
+const updateCategory = async (req, res) => {
+  const { name, description, category_image } = req.body;
+  const data = await sql`UPDATE categories SET 
+    name=${name},
+    description=${description},
+    category_image=${category_image},;`;
+  console.log("DATA", data);
+  res.status(200).json({ message: "Update category success", user: data });
+};
+const deleteCategory = async (req, res) => {
+  const { uid } = req.params;
+  const data = await sql`DELETE FROM users WHERE uid=${uid}`;
+  console.log("DATA", data);
+  res.status(200).json({ message: "Deleted success", user: data });
+};
+
+module.exports = {
+  getAllCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
--- back-end/controllers/user-controller.js
+++ back-end/controllers/user-controller.js
@@ -1,15 +1,36 @@
 const sql = require("../config/db");
 
 const getAllUser = async (req, res) => {
-  const data = await sql`SELECT * FROM employees`;
+  const data = await sql`SELECT * FROM users`;
   console.log("DATA", data);
   res.status(200).json({ message: "success", user: data });
 };
-const createUser = () => {};
-const updateUser = () => {};
+const createUser = async (req, res) => {
+  const { email, name, password, profile_img } = req.body;
+  const data = await sql`INSERT INTO users(email, name, password)
+VALUES (
+${email},
+${name},
+${password}
+${profile_img}
+);`;
+  console.log("DATA", data);
+  res.status(200).json({ message: "Create user success", user: data });
+};
+
+const updateUser = async (req, res) => {
+  const { email, name, password, profile_img } = req.body;
+  const data = await sql`UPDATE users SET profile_img=${profile_img},
+    email=${email},
+    name=${name},
+    password=${password},;`;
+  console.log("DATA", data);
+  res.status(200).json({ message: "User updated success", user: data });
+};
+
 const deleteUser = async (req, res) => {
   const { id } = req.params;
-  const data = await sql`DELETE FROM employees WHERE eid=${id}`;
+  const data = await sql`DELETE FROM users WHERE eid=${id}`;
   console.log("DATA", data);
   res.status(200).json({ message: "delete success", user: data });
 };
