47381c585bc77da702f8b78a6e8efaa84e10e8c8
dwjrgal <dwjrgl651@gmail.com>
2024-08-30 12:55:54 +0800

    front end

--- front-end/src/app/page.js
+++ front-end/src/app/page.js
@@ -1,8 +1,12 @@
 import Image from "next/image";
 import Login from "./components/login";
+import Signin from "./components/singin";
 
 export default function Home() {
   return (
-    <Login/>
+    <>
+      <Login />
+      <Signin />
+    </>
   );
 }
