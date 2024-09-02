773deaaa7651999a5407b31709bbe789c6c2d941
dwjrgal <dwjrgl651@gmail.com>
2024-09-02 21:54:23 +0800

    dashboard header

---
+++ front-end/src/app/dashboard/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const dashBoard = () => {
+  return (
+    <section>
+    <div>
+        <img src="./img/Header_logo.png" alt="" />
+        <h4>Dashboard</h4>
+        <p>Records</p>
+    </div>
+    <div>
+        <button>+ Record</button>
+        <img src="./img/Profile_img.png" alt="" />
+    </div>
+    </section>
+  )
+}
+
+export default dashBoard
