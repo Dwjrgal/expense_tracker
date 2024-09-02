4c2767c7f8c95e79b6d9f8600e785995ff65bd54
dwjrgal <dwjrgl651@gmail.com>
2024-09-02 21:33:11 +0800

    token

--- back-end/server.js
+++ back-end/server.js
@@ -5,6 +5,7 @@ const { logger } = require("./middlewares/logger");
 dotenv.config();
 
 const authRoutes = require("./routes/auth-route");
+const userRoutes = require("./routes/user-route");
 const categoryRoutes = require("./routes/category-route");
 const recordRoutes = require("./routes/record-route");
 
@@ -16,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(logger());
 
-app.user("/auth",authRoutes)
+app.use("/auth",authRoutes)
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/records", recordRoutes);
