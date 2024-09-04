15bfc45c394bd2b7abcc35918dfd5e9669e37b61
dwjrgal <dwjrgl651@gmail.com>
2024-09-04 21:16:15 +0800

    dashboard design

---
+++ front-end/src/app/components/income-frame.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+//  export const incomeFrame = () => {
+//   return (
+//     <div>
+//         <h4>Your income </h4>
+//         <h2>1,200,000</h2>
+//         <p>Your Income Amount </p>
+//         <span>32% from last month</span>
+
+//     </div>
+//   )
+// }
+
--- front-end/src/app/dashboard/page.jsx
+++ front-end/src/app/dashboard/page.jsx
@@ -5,6 +5,10 @@ import { UserContext } from "../context/user-context";
 import axios from "axios";
 // import { apiUrl } from "../../utils/util";
 import { toast } from "react-toastify";
+import  {incomeFrame}  from "../components"
+import { RxDotFilled } from "react-icons/rx";
+import { FaArrowCircleUp } from "react-icons/fa";
+
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
@@ -28,8 +32,16 @@ const Dashboard = () => {
 
   return (
     <section>
-      <div className="bg-slate-50">
+      <div className="bg-slate-50 flex gap-2 pt-5 ">
         <img src="./img/Card.png" alt="" className="w-60 h-32" />
+        <incomeFrame/>
+        <div className="border rounded-lg w-60 flex flex-col justify-center pl-3 bg-white">
+        <h4 className="border-b-[1px] border-gray-200 flex items-center gap-1 text-sm py-2 mb-2"><RxDotFilled className="text-xs text-green-500" /> Your income </h4>
+        <h2 className="text-3xl font-semibold">1,200,000</h2>
+        <p className="text-xs text-gray-500">Your Income Amount </p>
+        <span className="flex items-center gap-1 mt-2"> <FaArrowCircleUp className="text-green-500 text-sm" />32% from last month</span>
+
+    </div>
         {transactionData?.transactions?.map((transaction, index) => {
           return (
             <div key={index} className="flex">
