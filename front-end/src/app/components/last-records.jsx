3ca57babe2df8b2c2e51cd8ddb80f9518ea3dae9
dwjrgal <dwjrgl651@gmail.com>
2024-09-05 22:05:30 +0800

    records design

--- front-end/src/app/components/last-records.jsx
+++ front-end/src/app/components/last-records.jsx
@@ -1,25 +1,41 @@
 import React from "react";
-import { FaHome } from "react-icons/fa";
+import { PiPlusThin } from "react-icons/pi";
 
 const records = [
   {
     ctg_img:"./img/Home.png" ,
     title: "Lending & Renting",
     date: "3 hours ago",
+    expense: "-1000"
+  },
+  {
+    ctg_img:"./img/Home.png" ,
+    title: "Lending & Renting",
+    date: "3 hours ago",
+    expense: "-1000"
+  },
+  {
+    ctg_img:"./img/Home.png" ,
+    title: "Lending & Renting",
+    date: "3 hours ago",
+    expense: "-1000"
   },
 ];
 
 export const LastRecords = () => {
   return (
-    <section className="bg-white rounded pl-5 pt-3">
-      <h3 className="font-semibold pb-3">Last Records</h3>
+    <section className="bg-white rounded  pt-3">
+      <h3 className="font-semibold pb-3 pl-4">Last Records</h3>
       {records.map((records) => (
-        <div className="flex items-center border-solid border-t-2 border-gray gap-2 pt-4">
+        <div className="flex items-center justify-between border-solid border-t-2 border-gray gap-2 pt-4 pb-4 ml-4">
+          <div className="flex gap-3">
           <img className="h-8"src={records.ctg_img}></img>
           <div className="flex flex-col text-xs">
             <h4 className="font-semibold">{records.title}</h4>
             <p className="text-xs text-gray-500">{records.date}</p>
           </div>
+          </div>
+          <span className="text-green-400 pr-4 text-sm">{records.expense}</span>
         </div>
       ))}
     </section>
--- front-end/src/app/records/page.jsx
+++ front-end/src/app/records/page.jsx
@@ -1,32 +1,43 @@
+"use client"
+
 import React from "react";
 import { Header } from "../components";
+import { PiPlusThin } from "react-icons/pi";
+import { FaRegCircleDot } from "react-icons/fa6";
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
+
 
 const Records = () => {
   return(
      <>
     <Header/>
-    <section>
-      <h3>Records</h3>
-      <button>Add</button>
-      <input type="text" placeholder="Search" />
-      <div>
-        <h5>Types</h5>
-        <ul>
-          <li>All</li>
-          <li>Income</li>
-          <li>Expense</li>
+    <section className="flex gap-10 bg-slate-100">
+    <section className="w-60 bg-white rounded flex flex-col gap-4 pt-4 px-4 mt-5 ml-12">
+      <h3 className="text-2xl font-bold">Records</h3>
+      <button className="h-8 bg-blue-600 rounded-xl text-white  flex justify-center items-center gap-2"> <PiPlusThin className="text-white text-xl"/> Add</button>
+      <input type="text" placeholder="Search" className="w-52 rounded pl-2 bg-slate-100 border"/>
+      <div className="flex flex-col gap-3">
+        <h5 className="font-semibold">Types</h5>
+        <ul className="text-md font-normal pl-5">
+          <li className="flex gap-2 items-center"> <FaRegCircleDot />All</li>
+          <li className="flex gap-2 items-center"> <FaRegCircleDot />Income</li>
+          <li className="flex gap-2 items-center"> <FaRegCircleDot />Expense</li>
         </ul>
       </div>
-      <div>
-        <h5>Category</h5>
+      <div className="flex justify-between">
+        <h5 className="font-semibold">Category</h5>
+        <p className="text-gray-500">Clear</p>
       </div>
     </section>
-    <section>
-      <h5>Last 30 Days </h5>
-      <p>Today</p>
+    <section className="pt-5">
+      <h5 className="flex items-center gap-2 font-normal mb-5"> <button className="w-6 h-5 bg-white text-xs pl-1 rounded-md"><SlArrowLeft className="font-bold"/></button>
+        Last 30 Days <button className="w-6 h-5 bg-white text-xs pl-1 rounded-md"><SlArrowRight/></button> </h5>
+      <p className="font-semibold">Today</p>
       <div>
-        
+
       </div>
+    </section>
     </section>
   </>
   )
