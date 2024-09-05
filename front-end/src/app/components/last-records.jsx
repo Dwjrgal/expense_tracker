2937eacd4148a4d36cc5a1d6b0e018f245a3c605
dwjrgal <dwjrgl651@gmail.com>
2024-09-05 20:33:02 +0800

    records design

--- front-end/src/app/components/last-records.jsx
+++ front-end/src/app/components/last-records.jsx
@@ -3,7 +3,7 @@ import { FaHome } from "react-icons/fa";
 
 const records = [
   {
-    ctg_icon: <FaHome />,
+    ctg_img:"./img/Home.png" ,
     title: "Lending & Renting",
     date: "3 hours ago",
   },
@@ -11,14 +11,14 @@ const records = [
 
 export const LastRecords = () => {
   return (
-    <section>
-      <h3>Last Records</h3>
+    <section className="bg-white rounded pl-5 pt-3">
+      <h3 className="font-semibold pb-3">Last Records</h3>
       {records.map((records) => (
-        <div className="flex items-center">
-          <span className="text-blue-600">{records.ctg_icon}</span>
+        <div className="flex items-center border-solid border-t-2 border-gray gap-2 pt-4">
+          <img className="h-8"src={records.ctg_img}></img>
           <div className="flex flex-col text-xs">
-            <h4>{records.title}</h4>
-            <p>{records.date}</p>
+            <h4 className="font-semibold">{records.title}</h4>
+            <p className="text-xs text-gray-500">{records.date}</p>
           </div>
         </div>
       ))}
--- front-end/src/app/records/page.jsx
+++ front-end/src/app/records/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Header } from "../components";
 
 const Records = () => {
-  return <div></div>;
+  return(
+     <>
+    <Header/>
+    <section>
+      <h3>Records</h3>
+      <button>Add</button>
+      <input type="text" placeholder="Search" />
+      <div>
+        <h5>Types</h5>
+        <ul>
+          <li>All</li>
+          <li>Income</li>
+          <li>Expense</li>
+        </ul>
+      </div>
+      <div>
+        <h5>Category</h5>
+      </div>
+    </section>
+    <section>
+      <h5>Last 30 Days </h5>
+      <p>Today</p>
+      <div>
+        
+      </div>
+    </section>
+  </>
+  )
 };
 
 export default Records;
